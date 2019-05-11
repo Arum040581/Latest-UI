4a60f4a1e8ddf59e5bd6fd5c3f4b2267b5e66b1f
unknown <arumugam.msc28@yahoo.com>
2019-05-11 18:36:30 +0530

    latest

---
+++ myAngularApp-master/src/app/viewall/my-filter.pipe.ts
@@ -0,0 +1,23 @@
+import { Injectable, Pipe, PipeTransform } from '@angular/core';
+import { Workoutcollection } from "../models/workoutcollection";
+@Pipe({
+ name: 'filter',
+ pure: false
+})
+
+@Injectable()
+export class MyFilterPipe implements PipeTransform {
+  transform(items: any[], filtertext: any): any {        		 
+		if(filtertext.value != undefined  && items != undefined){	
+			console.log("aasdd");
+			console.log(items[0].workoutTitle);
+			if(items[0].workoutTitle != undefined)
+				return items.filter(item => item.workoutTitle.indexOf(filtertext.value) !== -1);            
+			else
+				return items.filter(item => item.categoryName.indexOf(filtertext.value) !== -1);            
+			
+		}
+		 return items;
+       
+    }
+}
